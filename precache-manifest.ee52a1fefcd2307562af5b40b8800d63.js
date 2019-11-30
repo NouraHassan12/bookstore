39f8a2ee33bb02b11871638c8feb2bf2e404d887
unknown <NouraHassan12/taskAPP.github>
2019-11-30 09:20:48 -0800

    Updates

---
+++ precache-manifest.ee52a1fefcd2307562af5b40b8800d63.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3348f3b61c31a27dc55a240aac5f186",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "3718ee94270c7cb5030e",
+    "url": "/bookstore/static/css/main.8eefbce1.chunk.css"
+  },
+  {
+    "revision": "ebea99ab64a761092269",
+    "url": "/bookstore/static/js/2.a2966d89.chunk.js"
+  },
+  {
+    "revision": "3718ee94270c7cb5030e",
+    "url": "/bookstore/static/js/main.2ab76053.chunk.js"
+  },
+  {
+    "revision": "e72c9a44a6dfa95c6904",
+    "url": "/bookstore/static/js/runtime-main.030d059f.js"
+  },
+  {
+    "revision": "61e267f309a44de648f8d2e112a7971b",
+    "url": "/bookstore/static/media/7k.61e267f3.jpg"
+  },
+  {
+    "revision": "0300effb48a8c9924e9a922b072c32a2",
+    "url": "/bookstore/static/media/92.0300effb.jpg"
+  },
+  {
+    "revision": "8190a2420d80a3919dd85fd819dbe03a",
+    "url": "/bookstore/static/media/bookcover1.8190a242.jpg"
+  },
+  {
+    "revision": "b4c9d86acab4e84f4b16fb2ce3b3df80",
+    "url": "/bookstore/static/media/lj.b4c9d86a.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/bookstore/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "5bffb506cf18dfd7df0af6edfa430049",
+    "url": "/bookstore/static/media/mn.5bffb506.jpg"
+  },
+  {
+    "revision": "7c353a086a447b4f460592c409ca7772",
+    "url": "/bookstore/static/media/novell222.7c353a08.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/bookstore/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "f8dd4092f7bc87100e08047bf9555a19",
+    "url": "/bookstore/static/media/rrr.f8dd4092.jpg"
+  }
+]);
